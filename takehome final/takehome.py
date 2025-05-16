50ec71ab5f20299ffd033a9408ecc93444668ee5
Tangalang22 <ntang@emfusion.net>
2025-05-16 15:43:06 -0700

    test of pulling data

--- takehome final/takehome.py
+++ takehome final/takehome.py
@@ -2,14 +2,16 @@ import requests
 import xmltodict
 import json
 import random
+import threading
+import logging
+import csv
+import os
 
 rates = ["EUR", "GBP", "USD", "DZD", "AUD", "BWP", "BND", "CAD", "CLP", "CNY", "COP", "CZK", "DKK", "HUF", "ISK", "INR", "IDR", "ILS", "KZT", "KRW", "KWD", "LYD", "MYR", "MUR", "NPR", "NZD", "NOK", "OMR", "PKR", "PLN", "QAR", "RUB", "SAR", "SGD", "ZAR", "LKR", "SEK", "CHF", "THB", "TTD"]
 ratesForBase = [r for r in rates if r != "USD" and r != "EUR" and r != "GBP"]
 
 def pulldata(base, date):
     # URL of the XML data
-    date = "2011-05-04"
-    base = random.choice(ratesForBase)
     url = f"https://www.floatrates.com/historical-exchange-rates.html?operation=rates&pb_id=1775&page=historical&currency_date={date}&base_currency_code={base}&format_type=xml"
     print(url)
     # Fetch the XML data
@@ -28,3 +30,7 @@ def pulldata(base, date):
     # Optionally, write the JSON data to a file
     with open(f"{date}_exchange_rates_{base}.json", "w") as json_file:
         json_file.write(json_data)
+
+date = "2011-05-04"
+base = random.choice(ratesForBase)
+pulldata(base, date)
