6820ab447f2c47bf22421a92aa72d59f65a63a04
Tangalang22 <ntang@emfusion.net>
2025-05-16 19:47:47 -0700

    part 1 fixing

--- takehome final/takehome.py
+++ takehome final/takehome.py
@@ -18,23 +18,34 @@ def pulldata(base, date):
 
     # URL of the XML data
     url = f"https://www.floatrates.com/historical-exchange-rates.html?operation=rates&pb_id=1775&page=historical&currency_date={date}&base_currency_code={base}&format_type=xml"
-    print(url)
+    # print(url)
     # Fetch the XML data
     response = requests.get(url)
     response.raise_for_status()  # Ensure we notice bad responses
 
-    # Parse the XML data to a Python dictionary
-    data_dict = xmltodict.parse(response.text)
+    try:
+        # Parse the XML data to a Python dictionary
+        data_dict = xmltodict.parse(response.text)
 
-    # Convert the dictionary to a JSON string
-    json_data = json.dumps(data_dict, indent=4)
+        # Convert the dictionary to a JSON string
+        json_data = json.dumps(data_dict, indent=4)
 
-    # Print the JSON data
-    print(json_data)
+        # Print the JSON data
+        # print(json_data)
 
-    # Optionally, write the JSON data to a file
-    with open(f"{directories}//{date}_exchange_rates_{base}.json", 'w') as json_file:
-        json_file.write(json_data)
+        # Optionally, write the JSON data to a file
+        with open(f"{directories}//{date}_exchange_rates_{base}.json", 'w') as json_file:
+            json_file.write(json_data)
+    except Exception:
+        print(url)
+        return 
+
+def sewingmachine(base, date): #get it, threads hehe
+    threadsPool.acquire()
+    # print(f"Thread {base} is starting")
+    pulldata(base,date)
+    # print(f"Thread {base}: finishing")
+    threadsPool.release()
 
 def increment_date(startdate, enddate):
     startobject = datetime.strptime(startdate, "%Y-%m-%d")
@@ -48,16 +59,22 @@ def increment_date(startdate, enddate):
     return dates
 
 if __name__ == "__main__":
-    date = "2011-05-04"
-    base = random.choice(ratesForBase)
-    pulldata(base, date)
-    #daylist = increment_date(date, today)
+    
+    max_threads = 1000
+    threadsPool = threading.Semaphore(max_threads)
 
-    threads = []
-    for x in increment_date(date, "2025-05-16"):
-        thread = threading.Thread(target=pulldata, args=(base,x,))
-        threads.append(thread)
-        thread.start()
+    date = "2011-05-04"
+    bases = random.sample(ratesForBase, 5)
+
+    for base in bases:
+        # pulldata(base, date)
+        #daylist = increment_date(date, today)
+
+        threads = []
+        for x in increment_date(date, "2025-05-16"):
+            thread = threading.Thread(target=sewingmachine, args=(base,x,))
+            threads.append(thread)
+            thread.start()
 
     #for day in daylist:
     #    pulldata(base, day)
